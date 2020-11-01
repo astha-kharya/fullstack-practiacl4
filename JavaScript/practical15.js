aa7c3125090de7e4165162a8448e0e848969ebad
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-11-01 18:19:00 +0530

    done

---
+++ JavaScript/practical15.js
@@ -0,0 +1,7 @@
+var decre = document.getElementById("left-button");
+var incre = document.getElementById("right-button");
+
+function incre() {
+    decre.disabled = true;
+    decre.style.backgroundColor = "rgb(4, 4, 4,0.2)";
+}

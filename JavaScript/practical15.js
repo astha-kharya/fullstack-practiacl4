4b15d14d8caab669fffd31a6f18ba7bb30672c4f
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-11-01 18:05:51 +0530

    deleting done

--- JavaScript/practical15.js
+++
@@ -1,7 +0,0 @@
-var decrement = document.getElementById("left button");
-var increment = document.getElementById("right button");
-
-function increment() {
-    decrement.disabled = true;
-    decrement.style.backgroundColor = "rgb(80,130,200)";
-}

f7d674e8a63b722e29b0a191c56728f6b74c63bc
astha-kharya <astha.kharya_cs18@gla.ac.in>
2020-11-07 16:08:22 +0530

    done

--- JavaScript/practical17.js
+++ JavaScript/practical17.js
@@ -21,5 +21,45 @@ function updateTime() {
         clock.style.color = "white";
         greeting.style.color = "white";
         greeting.innerHTML = 'GOOD NIGHT';
+    } else if (hours >= 16 && hours < 20) {
+        document.body.style.backgroundImage = "url('../img/evening.jpg')";
+        clock.style.color = "white";
+        greeting.innerHTML = 'GOOD EVENING';
+    } else if (hours >= 12 && hours < 16) {
+        document.body.style.backgroundImage = "url('../img/afternoon.jpg')";
+        clock.style.color = "black";
+        greeting.style.color = "white";
+        greeting.innerHTML = 'GOOD AFTERNOON';
+    } else {
+        document.body.style.backgroundImage = "url('../img/morning.jpg')";
+        clock.style.color = "black";
+        greeting.style.color = "black";
+        greeting.innerHTML = 'GOOD MORNING';
+    }
+    setTimeout(updateTime, 1000);
+}
+updateTime();
+getName();
+
+function addZero(n) {
+    return parseInt(n) < 10 ? '0' + n : n;
+};
+
+function setName(e) {
+    if (e.type === 'keypress') {
+        if (e.keyCode === 13) {
+            localStorage.setItem("name", e.target.innerHTML);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem("name", e.target.innerHTML);
+    }
+};
+
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.innerHTML = '[ASTHA KHARYA]';
+    } else {
+        name.innerHTML = localStorage.getItem('name');
     }
 }
